4036508ba8218fc9d1d566a5ea5e25d7c945bc26
Fabio <thebloodshadow14@gmail.com>
2023-11-15 14:24:45 +0100

    SETUP - macro structures

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script>
+import NavBar from "./components/NavBar.vue";
+import MainHandler from "./components/MainContent/MainHandler.vue";
+import Footer from "./components/Footer.vue";
+
 export default {
+  components: {
+    NavBar,
+    MainHandler,
+    Footer,
+  },
   data() {
     return {};
   },
 };
 </script>
 
-<template></template>
+<template>
+  <NavBar />
+  <MainHandler />
+  <Footer />
+</template>
--- src/components/MainContent/MainHandler.vue
+++ src/components/MainContent/MainHandler.vue
@@ -1,9 +1,14 @@
 <script>
+import HeroSection from "./MainSections/HeroSection.vue";
+
 export default {
   data() {
     return {};
   },
+  components: { HeroSection },
 };
 </script>
 
-<template></template>
+<template>
+  <HeroSection />
+</template>
