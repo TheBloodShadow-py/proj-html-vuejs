1ee3f51e1421055fb39594c71a747e547f617897
Fabio <thebloodshadow14@gmail.com>
2023-11-17 23:25:19 +0100

    ADD - LetStayInTouch

---
+++ src/components/MainContent/sections/LetStayInTouch.vue
@@ -0,0 +1,32 @@
+<script>
+import Button from "../../utility/Button.vue";
+
+export default {
+  components: { Button },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <section class="px-8 pb-[160px]">
+    <div class="container my-0 mx-auto transition-all duration-150">
+      <div class="grid lg:grid-cols-4 lg:grid-rows-2 gap-8">
+        <img class="rounded drop-shadow-md hidden lg:block" draggable="false" src="/social-1.jpg" />
+        <div class="col-span-2 rounded drop-shadow-md flex items-center justify-center bg-[#f1eef5]">
+          <div class="flex flex-col gap-4 text-center items-center p-12 lg:p-0">
+            <span class="other-font text-xs uppercase">Made for sharing</span>
+            <h2 class="text-brand select-text text-3xl font-bold max-w-[490px] md:max-w-[700px] lg:max-w-[490px] mb-7">Let's stay in touch</h2>
+            <Button :buttonText="'Follow Us'" :isLightButton="false" />
+          </div>
+        </div>
+        <img class="rounded drop-shadow-md hidden lg:block" draggable="false" src="/social-2.jpg" />
+        <img class="rounded drop-shadow-md hidden lg:block" draggable="false" src="/social-3.jpg" />
+        <img class="rounded drop-shadow-md hidden lg:block" draggable="false" src="/social-4.jpg" />
+        <img class="rounded drop-shadow-md hidden lg:block" draggable="false" src="/social-5.jpg" />
+        <img class="rounded drop-shadow-md hidden lg:block" draggable="false" src="/social-6.jpg" />
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/components/MainContent/sections/VisitOurBakeries.vue
@@ -0,0 +1,67 @@
+<script>
+import Button from "../../utility/Button.vue";
+
+export default {
+  components: {
+    Button,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <section class="px-8 pb-[160px]">
+    <div class="container my-0 mx-auto transition-all duration-150">
+      <div class="flex flex-col items-center gap-6 mb-8">
+        <span class="other-font uppercase text-xs text-center">Locations</span>
+        <h2 class="text-brand text-center select-text text-3xl font-bold max-w-[490px] md:max-w-[700px] lg:max-w-[490px] mb-7">Visit our Backeries</h2>
+      </div>
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-y-5">
+        <div class="flex flex-col">
+          <img class="drop-shadow-md rounded-t" draggable="false" src="/new-york-bk-1200x794.jpg" />
+          <div class="flex flex-col bg-[#f1f9fb] px-12 py-8 pt-0 rounded-b">
+            <div class="flex my-10 items-center justify-center">
+              <h2 class="text-brand text-center select-text text-3xl font-bold max-w-[490px] md:max-w-[700px] lg:max-w-[490px]">New York</h2>
+            </div>
+            <ul class="flex justify-between items-center">
+              <li class="flex flex-col items-center gap-3 text-center">
+                <span class="uppercase other-font text-xs">Call us</span>
+                <span class="text-xs other-font">1.800.458.556</span>
+              </li>
+              <li class="flex flex-col gap-3 text-center">
+                <span class="uppercase other-font text-xs">Open all week</span>
+                <span class="text-xs other-font">9:00 AM - 6:00 PM</span>
+              </li>
+              <li class="flex flex-col gap-3 text-center">
+                <Button :buttonText="'View Map'" :isLightButton="true" />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="flex flex-col">
+          <img class="drop-shadow-md rounded-t" draggable="false" src="/london-bk-1200x794.jpg" />
+          <div class="flex flex-col bg-[#faf3e3] px-12 py-8 pt-0 rounded-b">
+            <div class="flex my-10 items-center justify-center">
+              <h2 class="text-brand text-center select-text text-3xl font-bold max-w-[490px] md:max-w-[700px] lg:max-w-[490px]">London</h2>
+            </div>
+            <ul class="flex justify-between items-center">
+              <li class="flex flex-col items-center gap-3 text-center">
+                <span class="uppercase other-font text-xs">Call us</span>
+                <span class="text-xs other-font">1.800.458.556</span>
+              </li>
+              <li class="flex flex-col gap-3 text-center">
+                <span class="uppercase other-font text-xs">Open all week</span>
+                <span class="text-xs other-font">9:00 AM - 6:00 PM</span>
+              </li>
+              <li class="flex flex-col gap-3 text-center">
+                <Button :buttonText="'View Map'" :isLightButton="true" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
